1b69ccc969329478c7b8a363c380f669294f1513
mimimarsala <stephaniemarsala08@gmail.com>
2023-01-20 16:03:38 -0300

    cambios en la config de la db

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(
-  "peliculas_4hix",
-  peliculas_4hix_user,
-  x6ywL1kHJEJCQERRCftD2Gi9MOwMXqmz,
+  "peliculas_blj2",
+  peliculas_blj2_user,
+  J49NCPSHGZf7PUAvHpC8JgwjhDZbUFDx,
   {
-    host: "dpg-cf5buq9mbjsobakq8esg-a",
+    host: "dpg-cf5e9kirrk09v0vhc0vg-a",
     dialect: "postgres",
     logging: false,
   }
